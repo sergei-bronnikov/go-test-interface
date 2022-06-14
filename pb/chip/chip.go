188e89edb922d401bcc9db47db4077827269b547
sergei-bronnikov <sergeibronnikov87@gmail.com>
2022-06-14 11:29:28 +0300

    wip

--- pb/chip/chip.go
+++ pb/chip/chip.go
@@ -5,3 +5,7 @@ import "fmt"
 func TestChip(s string) {
 	fmt.Printf("test Chip: %s\n", s)
 }
+
+func TestChip2(s string) {
+	fmt.Printf("test Chip2: %s\n", s)
+}
--- test.go
+++
@@ -1,7 +0,0 @@
-package test
-
-import "fmt"
-
-func Test(s string) {
-	fmt.Printf("test: %s\n", s)
-}
