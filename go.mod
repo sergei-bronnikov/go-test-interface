848619488ca2327efd49a5b0c7e3603aef6a30f5
sergei-bronnikov <sergeibronnikov87@gmail.com>
2022-06-17 09:22:57 +0300

    wip

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sergei-bronnikov/go-test-interface/v111_test
+
+go 1.18
---
+++ proto/test.go
@@ -0,0 +1,7 @@
+package proto
+
+import "fmt"
+
+func Test(arg string) {
+	fmt.Printf("v111_test.1: %s\n", arg)
+}
