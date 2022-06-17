e5e3aa7e682d349176061a719c8c1e8117d4f387
sergei-bronnikov <sergeibronnikov87@gmail.com>
2022-06-17 09:50:53 +0300

    22_test.1.1 ...

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sergei-bronnikov/go-test-interface/22_test
+
+go 1.18
---
+++ proto/test.go
@@ -0,0 +1,7 @@
+package proto
+
+import "fmt"
+
+func Test(arg string) {
+	fmt.Printf("22_test.1.1: %s\n", arg)
+}
